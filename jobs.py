204d993bcb174f588da7f0064f71f086f4f943a7
Adrian Julius Aluoch <adrianjuliusaluoch@gmail.com>
2026-09-11 10:32:10 +0300

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -34,7 +34,7 @@ for location in locations:
         "location": str(location),
         "google_domain": "google.com",
         "hl": "en",
-        # "gl": "ke",
+        "no_cache": "true",
         "api_key": os.getenv("SERPAPI_KEY")
     }
 
